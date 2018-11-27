1784931402f670f721dd89bbf11be8174baeb987
Edgar Hirama <eh@ubby.io>
2018-11-27 10:55:27 -0200

    Initial commit + TMDb data models

---
+++ MovieAddiction/MovieAddiction/Models/Movie.swift
@@ -0,0 +1,46 @@
+//
+//  Movie.swift
+//  MovieAddiction
+//
+//  Created by Edgar Hirama on 27/11/18.
+//  Copyright © 2018 Edgar. All rights reserved.
+//
+
+import Foundation
+
+struct Movie: Codable {
+    let voteCount, id: Int
+    let video: Bool
+    let voteAverage: Double
+    let title: String
+    let popularity: Double
+    let posterPath: String
+    let originalLanguage: OriginalLanguage
+    let originalTitle: String
+    let genreIDS: [Int]
+    let backdropPath: String
+    let adult: Bool
+    let overview, releaseDate: String
+    
+    enum CodingKeys: String, CodingKey {
+        case voteCount = "vote_count"
+        case id = "id"
+        case video = "video"
+        case voteAverage = "vote_average"
+        case title = "title"
+        case popularity = "popularity"
+        case posterPath = "poster_path"
+        case originalLanguage = "original_language"
+        case originalTitle = "original_title"
+        case genreIDS = "genre_ids"
+        case backdropPath = "backdrop_path"
+        case adult = "adult"
+        case overview = "overview"
+        case releaseDate = "release_date"
+    }
+}
+
+enum OriginalLanguage: String, Codable {
+    case en = "en"
+    case ja = "ja"
+}
---
+++ MovieAddiction/MovieAddiction/Models/TMDbResponse.swift
@@ -0,0 +1,39 @@
+//
+//  TMDbResponse.swift
+//  MovieAddiction
+//
+//  Created by Edgar Hirama on 27/11/18.
+//  Copyright © 2018 Edgar. All rights reserved.
+//
+
+import Foundation
+
+// To parse the JSON, add this file to your project and do:
+//
+//   let welcome = try? newJSONDecoder().decode(Welcome.self, from: jsonData)
+
+import Foundation
+
+struct TMDbResponse: Codable {
+    let results: [Movie]
+    let page, totalResults: Int
+    let dates: Dates
+    let totalPages: Int
+    
+    enum CodingKeys: String, CodingKey {
+        case results = "results"
+        case page = "page"
+        case totalResults = "total_results"
+        case dates = "dates"
+        case totalPages = "total_pages"
+    }
+}
+
+struct Dates: Codable {
+    let maximum, minimum: String
+    
+    enum CodingKeys: String, CodingKey {
+        case maximum = "maximum"
+        case minimum = "minimum"
+    }
+}
